1bcb5dbd355d3e8946a8f45749ebd1045728e115
markymark5127 <markmayne@ymail.com>
2019-05-24 11:37:20 -0400

    Not finished
    real quick notes did this in like 2 mins obviously not done

--- install.sh
+++ install.sh
@@ -1,1 +1,28 @@
+#!/bin/bash
 
+#get gmail account
+read -p "Enter Your Gmail eMail: "  email
+read -p "Enter Your Gmail Password: "  pass
+read -p "Enter Your Gmail App Password: "  appPass
+
+read -p "Enter Your the \"to\" cel phone number (Ex: 4105555555)"  number
+read -p "Enter Your this numbers carrier (Ex: ATT)"  carrier
+
+read -p "do you want to dd another phone number?"  cur
+
+while [  $cur = "yes"]
+
+do 
+        read -p "Enter Your the \"to\" cel phone number (Ex: 4105555555)"  number
+        read -p "Enter Your this numbers carrier (Ex: ATT)"  carrier
+        read -p "do you want to dd another phone number?"  cur
+done 
+sudo apt-get install ssmtp mail utils
+
+edit etc/ssmtp/ssmtp.conf
+
+mailhub=smtp.gmail.com:587
+AuthUser = youremail
+AuthPass = yourpassword
+UseSTARTTLS=YES
+UseTLS=YES
